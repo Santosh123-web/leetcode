001bac7038c7b4e6be8dd6548cec167e26f87b7f
Santosh123-web <24244056@drngpit.ac.in>
2026-07-16 16:11:24 +0530

    Create 61-RotateList

---
+++ 61-RotateList/61-RotateList.java
@@ -0,0 +1,28 @@
+// Last updated: 7/16/2026, 4:10:14 PM
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) return head;
+
+        int length = 1;
+        ListNode dummy = head;
+
+        while (dummy.next != null) {
+            dummy = dummy.next;
+            length++;
+        }
+
+        int position = k % length;
+        if (position == 0) return head;
+
+        ListNode current = head;
+        for (int i = 0; i < length - position - 1; i++) {
+            current = current.next;
+        }
+
+        ListNode newHead = current.next;
+        current.next = null;
+        dummy.next = head;
+
+        return newHead;        
+    }
+}
