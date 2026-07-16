46f31779c33e4c52bc9d1c69a55567c20a32fe14
Santosh123-web <24244056@drngpit.ac.in>
2026-07-16 16:09:02 +0530

    Create 197-RisingTemperature

---
+++ 197-RisingTemperature/197-RisingTemperature.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/16/2026, 4:07:51 PM
+# Write your MySQL query statement below
+SELECT current_day.id
+FROM Weather AS current_day
+WHERE EXISTS (
+    SELECT 1
+    FROM Weather AS yesterday
+    WHERE current_day.temperature > yesterday.temperature
+    AND current_day.recordDate = yesterday.recordDate + INTERVAL 1 DAY
+);
