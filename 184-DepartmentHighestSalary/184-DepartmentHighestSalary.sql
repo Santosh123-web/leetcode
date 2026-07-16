7488b4ecd64e9276f90356936a15bdaa9f365c78
Santosh123-web <24244056@drngpit.ac.in>
2026-07-16 16:09:11 +0530

    Create 184-DepartmentHighestSalary

---
+++ 184-DepartmentHighestSalary/184-DepartmentHighestSalary.sql
@@ -0,0 +1,12 @@
+-- Last updated: 7/16/2026, 4:08:01 PM
+# Write your MySQL query statement below
+SELECT d.name AS Department, 
+       e.name AS Employee, 
+       e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    FROM Employee
+    WHERE departmentId = e.departmentId
+);
