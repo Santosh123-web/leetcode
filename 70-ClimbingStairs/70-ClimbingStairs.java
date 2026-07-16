71909631d5f1daa29684d5bc45896c338cc418ea
Santosh123-web <24244056@drngpit.ac.in>
2026-07-16 16:11:11 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -0,0 +1,16 @@
+// Last updated: 7/16/2026, 4:10:00 PM
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        int k = climbStairsHelper(n,dp);
+        return k;
+    }
+    public int climbStairsHelper(int n, int[] dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = climbStairsHelper(n-1,dp) + 
+                climbStairsHelper(n-2,dp);
+        return dp[n];
+    }
+}
