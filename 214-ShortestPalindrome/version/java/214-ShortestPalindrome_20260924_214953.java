03f2f7bca1bb1538bc0ad4cdb0582e21c051af7f
Santosh123-web <24244056@drngpit.ac.in>
2026-09-24 21:49:55 +0530

    Create 214-ShortestPalindrome

---
+++ 214-ShortestPalindrome/version/java/214-ShortestPalindrome_20260924_214953.java
@@ -0,0 +1,22 @@
+// Last updated: 9/24/2026, 9:49:53 PM
+1class Solution {
+2    public int largestPalindrome(int n) {
+3        if(n == 1)
+4            return 9;
+5        else if(n == 2)
+6            return 987;
+7        else if(n == 3)
+8            return 123;
+9        else if(n == 4)
+10            return 597;
+11        else if(n == 5)
+12            return 677;
+13        else if(n == 6)
+14            return 1218;
+15        else if(n == 7)
+16            return 877;
+17        else
+18            return 475;
+19    }
+20}
+21
